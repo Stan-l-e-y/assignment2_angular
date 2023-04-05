83afeb4f96de1a31ebd7d9598bf55ce96a9324b6
Stanley Tsonev <stanleytsonev@hotmail.com>
2023-04-05 14:55:32 -0400

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +8,39 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router, private authService: AuthService) {}
 
   onLogin() {
-    //validate
-    this.router.navigate([`/employees`]);
+    if (!this.email) {
+      alert('Email missing!');
+      return;
+    }
+    if (!this.password) {
+      alert('Password missing!');
+      return;
+    }
+
+    const user = {
+      email: this.email,
+      password: this.password,
+    };
+
+    this.authService.login(user).subscribe(
+      ({ data }) => {
+        console.log(data.login);
+        if (data.login == 'invalid credentials') {
+          alert('invalid credentials');
+          return;
+        }
+        this.router.navigate([`/employees`]);
+      },
+      (error) => {
+        alert(error);
+      }
+    );
   }
 
   onRegister() {
